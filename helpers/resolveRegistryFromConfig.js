a2a8f33c9f2df27992c6f2dec3a98b1c8d7e34dc
Pavel Kondratenko <iam@kondratenko.me>
2019-11-22 20:40:43 +0300

    repair release without registry defaults

--- helpers/resolveRegistryFromConfig.js
+++ helpers/resolveRegistryFromConfig.js
@@ -1,12 +1,17 @@
 const execSyncProgressReturn = require('./execSyncProgressReturn');
 
 module.exports = function resolveRegistryFromConfig(config) {
-  const username = execSyncProgressReturn(`echo ${config.registry.username}`);
-  const host = execSyncProgressReturn(`echo ${config.registry.host}`);
-  const path = execSyncProgressReturn(`echo ${config.registry.path}`);
-  if (!username || !host || !path) {
+  if (!config || !config.registry) {
     return false;
   }
 
-  return { username, host, path };
+  const username = execSyncProgressReturn(`echo ${config.registry.username}`);
+  const password = config.registry.password;
+  const host = execSyncProgressReturn(`echo ${config.registry.host}`);
+  const path = execSyncProgressReturn(`echo ${config.registry.path}`);
+  if (!username || !password || !host || !path) {
+    return false;
+  }
+
+  return { username, password, host, path };
 };
